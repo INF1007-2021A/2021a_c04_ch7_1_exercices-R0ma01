65788aee2fd695bb6afb0bceed6200dbda71eca0
R0ma01 <romaine.brand@polymtl.ca>
2021-10-26 19:11:50 -0400

    Update exercice.py

--- exercice.py
+++ exercice.py
@@ -8,9 +8,10 @@
 from typing import Tuple
 import numpy as num
 
-def volume_elipsoide_2(a, b, c) -> Tuple:
+def volume_elipsoide_2(axe_a=1, axe_b=1, axe_c=1, mv = 1) -> Tuple[float, float]:
+    volume = (4/3)*num.pi*axe_a*axe_b*axe_c
 
-    return (4/3)*num.pi*a*b*c
+    return (volume, (mv*volume))
 
     #EXERCICE 2
 
@@ -18,12 +19,22 @@ def volume_elipsoide_2(a, b, c) -> Tuple:
 #écrivez un programme qui trie les lettres à partir du dictionnaire et qui retourne la lettre avec la fréquence la plus haute,
 #en utilisant une fonction lambda.
 
+def trier_lettres(phrase : str) -> Tuple[dict, list]:
+    dict =  {}
+
+    for lettre in phrase :
+        if not lettre in dict:
+            dict[lettre] = 1
+        else:
+            dict[lettre]+= 1
+
+    return dict, sorted(dict.items(), key = lambda item : item[1], reverse = True)[0][0]
 
 
 ## EXERCICE 3
 
 import turtle
-turtle.color("green")
+
 
 def recursive(chose):
 
@@ -41,24 +52,29 @@ def recursive(chose):
 
     ## EXERCICE 4
 
-def valide(chaine, sequence):
-    return (
-        True if chaine != None and sequence != None else
-        False
-    )
+def valide(chaine):
+    return bool(chaine) and all([lettre in "atgc" for lettre in chaine])
+
+def saisie() -> Tuple[str, str]:
+    chaine = None
+    while not valide(chaine):
+        chaine = input("veuillez entrer la chaine de charactères")
+        sequence = input("veuillez entrer la séquence à verrifier")
+
+    return chaine, sequence
+
+def adn():
+    chaine, sequence = saisie()
+
+    pourcentage  = round(((chaine.count(sequence)) / len(chaine) * 100),2)
+    return pourcentage
 
 
-def adn(adn, sequence):
-    pourcentage  = round(((adn.count(sequence)) / len(adn) * 100),2)
-    return (
-        pourcentage if valide(adn, sequence) else
-        False
-    )
 
 if __name__ == '__main__':
     # TODO: Appelez vos fonctions ici
     chaine = "attgcaatggtggtacatg"
     sequence = "ca"
-    print(adn(chaine, sequence))
+    print("il y a "+ str(adn())+ '% de"'+ sequence+'".')
 
 
